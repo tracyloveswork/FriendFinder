585efe2650ec478b71bd5796727eae71e1274f86
tracyloveswork <tracyloveswork@gmail.com>
2017-07-06 00:41:33 -0400

    Add files via upload

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -11,8 +11,52 @@ module.exports = function(app) {
 
 	// Add Friend to array and find match
   app.post("/api/friends", function(req, res) {
+  	
+  	// Find match
+  	var bestMatch = {};
+
+  	var best = 0;
+
+  	var currentFriend = null;
+
+		// Outer Loop
+  	for (var i = 0; i < friends.length; i++) {
+
+  		var differenceArray = [];
+
+  		// Inner loop
+  		for (var j = 0; j < friends[i].scores.length; j++) {
+  			var difference = Math.abs(friends[i].scores[j] - req.body.scores[j]);
+  			differenceArray.push(difference);
+  			console.log("User Score: " + req.body.scores[j] + " Versus: " + friends[i].scores[j]);
+  		};
+
+  		var total = differenceArray.reduce(function (a,b) {
+  			return a + b;
+  		}, 0);
+
+  		console.log("Difference total: " + total)
+
+  		if(currentFriend == null) {
+  			currentFriend = total;
+  		}
+
+  		else if (total < currentFriend) {
+  			best = i;
+  			currentFriend = total;
+  		}
+
+  	};
+
+  	bestMatch = friends[best];
+
+  	res.json(bestMatch);
+
+		// Add to array
     friends.push(req.body);
-    res.json(friends[0]);
+
+		// Return result placeholder
+    // res.json(friends[0]);
   });
 
 	};
